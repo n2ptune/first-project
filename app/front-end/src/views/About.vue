4a0000aff104c6fe2ac53c025541c78685029af3
n2ptune <n2ptune@outlook.com>
2020-03-30 18:42:11 +0900

    feat: Get user data from api server

---
+++ app/front-end/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    About
+  </div>
+</template>
+
+<style scoped>
+.about {
+  font-size: 2.25rem;
+}
+</style>
---
+++ app/front-end/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    Home
+  </div>
+</template>
+
+<style scoped>
+.home {
+  font-size: 2.25rem;
+}
+</style>
